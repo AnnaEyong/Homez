8706f87095ba0b8a407a8aab887d57724ddb7159
Ewoube Anna <ewoube.anna@gmail.com>
2025-05-14 11:41:41 +0100

    updates

--- src/components/filter/FilterComponent.jsx
+++ src/components/filter/FilterComponent.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function FilterComponent({name, icon}) {
   return (
 
-<button className='flex text-[13px] flex-col items-center justify-center text-gray-500'>
+<button className='flex text-[13px] flex-col items-center justify-center text-gray-500 hover:text-primary focus:text-primary'>
     <div className='cursor-pointer'>
       {icon}
     </div>
